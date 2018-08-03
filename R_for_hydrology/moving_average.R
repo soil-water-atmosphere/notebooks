d1062eff2ad6e8c6faf72b4f279002e659b34c8d
Claudia Brauer <claudia.brauer@wur.nl>
2018-08-03 16:46:38 +0200

    Add files via upload

--- R_for_hydrology/moving_average.R
+++ R_for_hydrology/moving_average.R
@@ -42,18 +42,23 @@ Qave = rollmean(Q, k=24, fill="extend")
 ### Plot original and moving average together
 
 # Open pdf
-pdf("figs/moving_average.pdf", width=4, height=3)
+pdf("figs/moving_average.pdf", width=4, height=3, family="Times")
+
+# Change margins (mar), distance of axes and labels (mgp) and tick marks (tcl).
+par(mar=c(2.4,2.4,0.1,0.1), mgp=c(1.2,0.2,0), tcl=-0.2) 
 
 # Plot discharge
-plot(d_feb2011$Q, type="l", col="dodgerblue")
+plot(d_feb2011$Q, type="l", col="dodgerblue", ylim=c(0,0.65),
+     xlab="time [h]", ylab=expression(paste("Q [mm h"^{-1},"]")),
+     xaxs="i", yaxs="i") # to remove space around the lines
 
 # Add moving average
 lines(Qave, col="darkblue")
 
 # Add legend
-legend(c("original","48-h moving average"), 
+legend(c("original","48h mov. ave."), 
        col=c("dodgerblue","darkblue"), 
-       bty="n", lty=1, x="topleft")
+       bty="n", lty=1, x="bottomleft")
 
 # Close pdf
 graphics.off()
